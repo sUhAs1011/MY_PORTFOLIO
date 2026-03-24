e61cd6fd77346167ab48792996bd860a3f0a60e8
SUHAS VENKATA KARAMALAPUTTI <113611352+sUhAs1011@users.noreply.github.com>
2026-03-24 15:49:18 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1537,25 +1537,25 @@ st.header("🏆 Achievements")
 
 achievements = [
     {
-    "title": "HealthHack Hackathon",
+    "title": "HealthHack 4.0 Hackathon",
     "icon": "🧠",
     "desc": "Secured a <strong>Top 8 position</strong> among 80+ registered teams at <strong>HealthHack 4.0</strong> for building Kalpana AI, an AI-driven mental health peer support platform.",
     "tag": "Hackathon Achievement"
     },
     {
-        "title": "Anveshana 2026",
+        "title": "Anveshana 3.0 2026",
         "icon": "🔍",
         "desc": "Ranked Top 11 out of 170 Capstone Teams for Detecting and Mitigating Replay Attacks in CCTV systems.",
         "tag": "Top 11 Finish"
     },
     {
-        "title": "Encode-AI Agentathon",
+        "title": "Encode-AI Agentathon 2.0",
         "icon": "🥈",
         "desc": "Achieved 2nd place for building a multi-agent Agentic AI incident investigation system using <strong>MCP</strong> and <strong>Ollama</strong>.",
         "tag": "Hackathon Runner-Up"
     },
     {
-        "title": "Heal-O-Code Hackathon",
+        "title": "Heal-O-Code 2.0 Hackathon",
         "icon": "🩺",
         "desc": "Top 6 out of 50+ teams. Built a healthcare decision support tool using <strong>Multi-Chain Blockchain</strong> and <strong>Ollama</strong> for better drug recommendation.",
         "tag": "Top 6 Finish"
