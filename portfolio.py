42a3d349a059cd565ac7fcafcca0b7fefa7a0dea
SUHAS VENKATA KARAMALAPUTTI <113611352+sUhAs1011@users.noreply.github.com>
2026-04-14 10:37:54 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1182,7 +1182,8 @@ tool_icon_map = {
     "CursorAI": "https://img.icons8.com/?size=100&id=DiGZkjCzyZXn&format=png&color=000000",
     "Jupyter": "https://img.icons8.com/?size=100&id=J0SgMWzAxqFj&format=png&color=000000",  # Using generic Jupyter icon
     "Google Colab": "https://img.icons8.com/?size=100&id=lOqoeP2Zy02f&format=png&color=000000",
-    "Github": "https://img.icons8.com/?size=100&id=SzgQDfObXUbA&format=png&color=000000"
+    "Github": "https://img.icons8.com/?size=100&id=SzgQDfObXUbA&format=png&color=000000",
+    "AntiGravity": "https://antigravity.google/press"
 }
 
 tools_list = list(tool_icon_map.keys())
