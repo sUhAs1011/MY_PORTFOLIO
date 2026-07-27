8da448b818708e9beead83dcbee49a109605c1ba
SUHAS VENKATA KARAMALAPUTTI <113611352+sUhAs1011@users.noreply.github.com>
2026-07-27 09:44:08 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -466,7 +466,7 @@ st.markdown("""
     <div class="timeline-content">
       <h3>B.Tech CSE</h3>
       <p>PES University</p>
-      <p>🗓️ 2022 - Present</p>
+      <p>🗓️ 2022 - 2026</p>
       <p>CGPA : 8.20</p>
     </div>
   </div>
