b405f56bcae9e2c35848dc3116714f07d3a9b5a7
SUHAS VENKATA KARAMALAPUTTI <113611352+sUhAs1011@users.noreply.github.com>
2026-08-23 10:53:44 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -200,8 +200,7 @@ with col1:
     unsafe_allow_html=True)
     st.markdown("<div class='subtitle'>CSE Senior at PES University | Software Engineer Intern at Epsilon</div>", unsafe_allow_html=True)
     st.markdown("""<p style='font-size:18px; line-height:1.6;'>
-Final-year Computer Science & Engineering student at PES University, currently working as a Software Engineer Intern at Epsilon. Previously worked as a Software Engineering Intern at Elfonze Technologies and as a Summer Research Intern at C3I, contributing to end-to-end AI and software solutions. Passionate about Machine Learning, Natural Language Processing, and applied AI, with hands-on experience building ML systems and solving real-world problems through software engineering and research-driven projects.
-</p>
+Computer Science graduate from PES University, currently working as an Associate Software Engineer at Epsilon. Previously worked as a Software Engineering Intern at Elfonze Technologies and as a Summer Research Intern at C3I, contributing to end-to-end AI and software solutions. Passionate about Machine Learning, Natural Language Processing, and Applied AI, with hands-on experience building intelligent systems and solving real-world problems through software engineering and research-driven projects.
 """, unsafe_allow_html=True)
     st.markdown("</div>", unsafe_allow_html=True)
 
